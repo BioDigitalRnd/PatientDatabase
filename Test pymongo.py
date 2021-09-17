c3ede893e2c957fce6f625a0c93837378bf47bc1
Aixzyl14 <71727630+Aixzyl14@users.noreply.github.com>
2021-09-17 12:32:30 +0100

    Update Test pymongo.py

--- Test pymongo.py
+++ Test pymongo.py
@@ -50,46 +50,46 @@ schedule = sched.scheduler(time.time, time.sleep)
 
 # schedule.enter(5, 1, agetimeseq, (schedule,))
 
-# def linefunc(num):
-#     list_of_lines = []
-#     count = 0
-#     file = open("Test_data.txt", "r")
-#     for line in file:
-#         stripped_line = line.strip()
-#         line_list = stripped_line.split()
-#         list_of_lines.append(line_list)
-#         count += 1
-#     return list_of_lines
+def linefunc(num):
+    list_of_lines = []
+    count = 0
+    file = open("Test_data.txt", "r")
+    for line in file:
+        stripped_line = line.strip()
+        line_list = stripped_line.split()
+        list_of_lines.append(line_list)
+        count += 1
+    return list_of_lines
 
-# num = 0
-# ntsqRan = False
+num = 0
+ntsqRan = False
 
-# def nametimeseq(nsc): #Changes the Name every interval currently 5 sec
-#     global num
-#     global ntsqRan
-#     print("name")
-#     func = linefunc(0)
-#     if num <= (len(func) - 1):
-#         sname = ""
-#         for x in func[num]:
-#             sname += "" + x
-#         print(sname)
-#         schedule.enter(5, 1, nametimeseq, (nsc,))
-#         if ntsqRan is False:
-#             oname = datab["name"] #old name        ||  once changed data is no longer retrievable fix by checking the array instead
-#             ntsqRan = True
-#         else:
-#             curname = ""
-#             for x in func[num - 1]:
-#                 curname += "" + x
-#                 oname =  curname
-#         cname = {"name": oname} #cur name
-#         nname = {"$set": {"name": sname}} # set new name
-#         coll.update_one(cname, nname)
-#         num += 1
+def nametimeseq(nsc): #Changes the Name every interval currently 5 sec
+    global num
+    global ntsqRan
+    print("name")
+    func = linefunc(0)
+    if num <= (len(func) - 1):
+        sname = ""
+        for x in func[num]:
+            sname += "" + x
+        print(sname)
+        schedule.enter(5, 1, nametimeseq, (nsc,))
+        if ntsqRan is False:
+            oname = datab["name"] #old name        ||  once changed data is no longer retrievable fix by checking the array instead
+            ntsqRan = True
+        else:
+            curname = ""
+            for x in func[num - 1]:
+                curname += "" + x
+                oname =  curname
+        cname = {"name": oname} #cur name
+        nname = {"$set": {"name": sname}} # set new name
+        coll.update_one(cname, nname)
+        num += 1
 
-# schedule.enter(5, 1, nametimeseq, (schedule,))
-# schedule.run()
+schedule.enter(5, 1, nametimeseq, (schedule,))
+schedule.run()
 
 # newid = itertools.count()
 
