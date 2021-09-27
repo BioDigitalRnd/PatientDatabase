a63d51d11cab71e860026c31d91402a048a95848
Aixzyl14 <71727630+Aixzyl14@users.noreply.github.com>
2021-09-27 16:28:44 +0100

    Update MachineL_Test.py

--- MachineL_Test.py
+++ MachineL_Test.py
@@ -18,7 +18,6 @@ from pandas import read_csv
 from pandas.plotting import scatter_matrix
 from matplotlib import pyplot
 
-#
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import StratifiedKFold
@@ -33,37 +32,43 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
 # Load dataset
-url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
-names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-dataset = read_csv(url, names=names)
+# url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+# names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+# dataset = read_csv(url, names=names)
+
+path = r"C:\Users\BioGuest\test\Sample.csv"
+tag = ['Slept well yesterday?', 'Yesterday was stressful?','Weight (kg)', 'Height (cm)']
+datset = read_csv(path, names=tag)
 
 # Split-out validation dataset
-array = dataset.values
-X = array[:,0:4]
-y = array[:,4]
+array = datset.values
+X = array[:, 1]
+y = array[1,:]
 X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
 
-# Spot Check Algorithms
-models = []
-models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr'))) # Logistic Regression (LR)
-models.append(('LDA', LinearDiscriminantAnalysis())) # Linear Discriminant Analysis (LDA)
-models.append(('KNN', KNeighborsClassifier())) # K-Nearest Neighbors (KNN).
-models.append(('CART', DecisionTreeClassifier())) # Classification and Regression Trees (CART).
-models.append(('NB', GaussianNB())) # Gaussian Naive Bayes (NB).
-models.append(('SVM', SVC(gamma='auto'))) # Support Vector Machines (SVM).
 
 
-# Make predictions on validation dataset
-model = SVC(gamma='auto')
-model.fit(X_train, Y_train)
-predictions = model.predict(X_validation)
+# # Spot Check Algorithms
+# models = []
+# models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr'))) # Logistic Regression (LR)
+# models.append(('LDA', LinearDiscriminantAnalysis())) # Linear Discriminant Analysis (LDA)
+# models.append(('KNN', KNeighborsClassifier())) # K-Nearest Neighbors (KNN).
+# models.append(('CART', DecisionTreeClassifier())) # Classification and Regression Trees (CART).
+# models.append(('NB', GaussianNB())) # Gaussian Naive Bayes (NB).
+# models.append(('SVM', SVC(gamma='auto'))) # Support Vector Machines (SVM).
 
-# Evaluate Predictions
-print(accuracy_score(Y_validation, predictions))
-print(confusion_matrix(Y_validation, predictions))
-print(classification_report(Y_validation, predictions))
 
-#
+# # Make predictions on validation dataset
+# model = SVC(gamma='auto')
+# model.fit(X_train, Y_train)
+# predictions = model.predict(X_validation)
+
+# # Evaluate Predictions
+# print(accuracy_score(Y_validation, predictions))
+# print(confusion_matrix(Y_validation, predictions))
+# print(classification_report(Y_validation, predictions))
+
+
 # #Evaluate each model in turn
 # results = []
 # names = []
